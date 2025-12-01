7abb1e63bd36ea1e79648733696bcee1d2d7ef44
Tom <tombruin26@hotmail.com>
2025-12-01 21:54:25 +0000

    day 1 Part 2 unoptimized

--- README.md
+++ README.md
@@ -11,9 +11,9 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `38.4µs` | `-` |
+| [Day 1](./src/bin/01.rs) | `54.3µs` | `57.3µs` |
 
-**Total: 0.04ms**
+**Total: 0.11ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/01.rs
+++ src/bin/01.rs
@@ -22,7 +22,37 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut vaultcode: i32 = 50;
+    let mut vaultzero: u64 = 0;
+
+    for line in input.lines() {
+        let (direction, rest) = line.split_at(1);
+        let number: i32 = rest.parse().unwrap();
+        vaultzero +=  (number).div_euclid(100).abs() as u64;
+        let restnumber = (number).rem_euclid(100);
+
+        if vaultcode == 0 { vaultzero += 1; }
+        match direction.as_bytes()[0] {
+            R => {
+                let wraps =  (vaultcode + restnumber).div_euclid(100);
+                // Skip 100
+                if wraps > 0 &&  (vaultcode + restnumber) != 100 {
+                    vaultzero += wraps as u64;
+                }
+                vaultcode =  (vaultcode + restnumber).rem_euclid(100);
+            },
+            L => {
+                let wraps = (vaultcode - restnumber).div_euclid(100).abs();
+                // Only if not 0
+                if wraps > 0 && vaultcode != 0 {
+                    vaultzero += wraps as u64;
+                }
+                vaultcode = (vaultcode - restnumber).rem_euclid(100);
+            },
+            _    => {}
+        }
+    }
+    return Some(vaultzero);
 }
 
 #[cfg(test)]
@@ -38,6 +68,8 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
+
 }
+
