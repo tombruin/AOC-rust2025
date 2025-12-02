912fc3008733da6f2f7c120075cc57687b3e53d7
Tom <tombruin26@hotmail.com>
2025-12-02 23:00:48 +0000

    Added part two for day 2

--- README.md
+++ README.md
@@ -12,8 +12,9 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
 | [Day 1](./src/bin/01.rs) | `55.7µs` | `58.7µs` |
+| [Day 2](./src/bin/02.rs) | `37.1ms` | `74.8ms` |
 
-**Total: 0.11ms**
+**Total: 112.01ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/02.rs
+++ src/bin/02.rs
@@ -12,7 +12,6 @@ pub fn has_repeated_sequence(productid: &str) -> bool {
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
-
     let sum_invalid_productids: u64 = input.trim()
         .split(',')
         .filter_map(|product_idpair| product_idpair.split_once('-'))
@@ -23,8 +22,42 @@ pub fn part_one(input: &str) -> Option<u64> {
     return Some(sum_invalid_productids);
 }
 
+
+pub fn has_repeated_sequence_twice(productid: &str) -> bool {
+    let productid_length = productid.len();
+    // Try all possible substring lengths from 1 untill half of the length
+    for sub_pattern_length in 1..=productid_length / 2 {
+        if productid_length % sub_pattern_length == 0 {
+            let mut valid = true;
+            // Take first possible repeated pattern
+            let first_pattern = &productid[0..sub_pattern_length];
+            // Check every chunk of size the repeated pattern
+            for i in (0..productid_length).step_by(first_pattern.len()) {
+                let endlength = i + sub_pattern_length;
+                let possible_pattern = &productid[i..endlength];
+                if possible_pattern != first_pattern {
+                    // Not the same pattern seen. So not valid
+                    valid = false;
+                    break;
+                }
+            }
+            if valid {
+                return true;
+            }
+        }
+    }
+    false
+}
+
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let sum_invalid_productids: u64 = input.trim()
+    .split(',')
+    .filter_map(|product_idpair| product_idpair.split_once('-'))
+    .map(|(startid, endid)| (startid.parse::<u64>().unwrap(), endid.parse::<u64>().unwrap()))
+    .flat_map(|product_idrange| (product_idrange.0..=product_idrange.1).filter(|&n| has_repeated_sequence_twice(&n.to_string())))
+    .sum();
+
+    return Some(sum_invalid_productids);
 }
 
 #[cfg(test)]
@@ -40,6 +73,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4174379265));
     }
 }
